0f172109a111cbe5e9094e7fdaf9bb55cee9e6ad
Tamas Gal <himself@tamasgal.com>
2018-07-17 14:02:19 +0200

    Update keys

--- config.h
+++ config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Aleo:size=13" };
-static const char dmenufont[]       = "Aleo:size=13";
+static const char *fonts[]          = { "Fira:size=8" };
+static const char dmenufont[]       = "Fira:size=8";
 
 static const char normbordercolor[] = "#f6f6f6";
 static const char normbgcolor[]     = "#f6f6f6";
@@ -23,8 +23,8 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = {
-  "base", "web", "dev", "doc", "img",
-  "vid", "mus", "eml", "foo",
+  "1-hme", "2-web", "3-dev", "4-doc", "5-img",
+  "6-vid", "7-eml", "8-phd", "9-foo",
 };
 
 static const Rule rules[] = {
@@ -40,17 +40,15 @@ static const Rule rules[] = {
   { "Vimb",          NULL,        NULL,          1 << 1,  False,       -1 },
   { "tabbed",        NULL,        NULL,          1 << 1,  False,       -1 },
   {  NULL,           NULL,        "evim",        1 << 2,  False,       -1 },
-  { "Zathura",       NULL,        NULL,          1 << 3,  False,       -1 },
-  { "Sxiv",          NULL,        NULL,          1 << 4,  False,       -1 },
-  { "Inkscape",      NULL,        NULL,          1 << 4,  False,       -1 },
+  { "Zathura",       NULL,        NULL,          1 << 4,  False,       -1 },
+  { "Sxiv",          NULL,        NULL,          1 << 5,  False,       -1 },
+  { "Inkscape",      NULL,        NULL,          1 << 5,  False,       -1 },
   { "mpv",           NULL,        NULL,          1 << 5,  False,       -1 },
-  { "VidyoDesktop",  NULL,        NULL,          1 << 5,  False,       -1 },
-  { "vncviewer",     "Vncviewer", NULL,          1 << 5,  False,       -1 },
-  {  NULL,           NULL,        "cmus",        1 << 6,  False,       -1 },
+  { "VidyoDesktop",  NULL,        NULL,          1 << 6,  False,       -1 },
+  { "vncviewer",     "Vncviewer", NULL,          1 << 6,  False,       -1 },
   {  NULL,           NULL,        "mutt",        1 << 7,  False,       -1 },
   {  NULL,           NULL,        "scratchpad",  0,       True,        -1 },
   {  NULL,           NULL,        "todo",        0,       True,        -1 },
-  {  NULL,           NULL,        "esp",        0,       True,         -1 },
   { "Gcolor2",       NULL,        NULL,          0,       True,        -1 },
   { "Lxappearance",  NULL,        NULL,          0,       True,        -1 },
   { "XFontSel",      NULL,        NULL,          0,       True,        -1 },
@@ -85,27 +83,19 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]       = { "kitty", NULL };
 static const char *irssicmd[]      = { "kitty", "-t", "irssi", "-e", "irssi", NULL };
-static const char *lockcmd[]       = { "slock", NULL };
-static const char *mailcmd[]       = { "kitty", "-t", "mutt", "-e", "mutt", NULL };
-static const char *muscmd[]        = { "st", "-t", "cmus", "-e", "cmus", NULL };
-static const char *musnext[]       = { "st", "-e", "cmus-remote", "--next", NULL };
-static const char *muspause[]      = { "st", "-e", "cmus-remote", "--pause", NULL };
-static const char *musprev[]       = { "st", "-e", "cmus-remote", "--prev", NULL };
-static const char *padcmd[]        = { "st", "-t", "scratchpad", "-g", "56x10-30+40", NULL };
-static const char *rangercmd[]     = { "st", "-t", "ranger", "-e", "ranger", NULL };
-static const char *tmuxcmd[]       = { "st", "-t", "tmux", "-e", "tmux", "-f", "/home/moritz/.tmux.conf", NULL };
-static const char *todocmd[]       = { "st", "-t", "todo", "-e", "vim", "/home/moritz/TODO", NULL };
-static const char *espcmd[]        = { "st", "-t", "esp", "-e", "vim", "/home/moritz/ESP", NULL };
+static const char *lockcmd[]       = { "blurlock", NULL };
+static const char *mailcmd[]       = { "kitty", "-T", "mutt", "-e", "mutt", NULL };
+static const char *padcmd[]        = { "kitty", "-T", "scratchpad", "-g", "56x10-30+40", NULL };
+static const char *rangercmd[]     = { "kitty", "-T", "ranger", "-e", "ranger", NULL };
+static const char *tmuxcmd[]       = { "kitty", "-T", "tmux", "-e", "tmux", "-f", "/home/tgal/.tmux.conf", NULL };
+static const char *todocmd[]       = { "kitty", "-T", "todo", "-e", "vim", "/home/tgal/TODO", NULL };
 static const char *voldowncmd[]    = { "pactl", "set-sink-volume", "0", "-2%", NULL };
 static const char *volupcmd[]      = { "pactl", "set-sink-volume", "0", "+2%", NULL };
 static const char *lightdowncmd[]    = { "xbacklight", "-10", NULL };
 static const char *lightupcmd[]      = { "xbacklight", "+10", NULL };
 static const char *voloffcmd[]     = { "pactl", "set-sink-mute", "0", "toggle", NULL };
-static const char *brightupcmd[]   = { "xbacklight", "-inc", "10", NULL };
-static const char *brightdowncmd[] = { "xbacklight", "-doc", "10", NULL };
 static const char *printcmd[]      = { "scrot", NULL };
 static const char *printfocuscmd[] = { "scrot", "--focused", NULL };
-static const char *webcmd[]        = { "/home/moritz/bin/run_browser", NULL };
 static const char *chromecmd[]     = { "chromium", NULL };
 
 static Key keys[] = {
@@ -138,22 +128,16 @@ static Key keys[] = {
   { 0,                 0x1008ff13,  spawn,           {.v = volupcmd } },
   { 0,                 0x1008ff03,  spawn,           {.v = lightdowncmd } },
   { 0,                 0x1008ff02,  spawn,           {.v = lightupcmd } },
-  { 0,                 0x1008ff14,  spawn,           {.v = muspause } },
-  { 0,                 0x1008ff16,  spawn,           {.v = musprev } },
-  { 0,                 0x1008ff17,  spawn,           {.v = musnext } },
-  { MODKEY|ShiftMask,  XK_v,        spawn,           {.v = webcmd} },
   { MODKEY|ShiftMask,  XK_w,        spawn,           {.v = chromecmd} },
   { MODKEY|ShiftMask,  XK_i,        spawn,           {.v = irssicmd} },
   { MODKEY|ShiftMask,  XK_l,        spawn,           {.v = lockcmd} },
   { MODKEY|ShiftMask,  XK_m,        spawn,           {.v = mailcmd} },
   { MODKEY|ShiftMask,  XK_s,        spawn,           {.v = padcmd} },
-  { MODKEY|ShiftMask,  XK_o,        spawn,           {.v = muscmd} },
   { MODKEY,            XK_p,        spawn,           {.v = printcmd} },
   { MODKEY|ShiftMask,  XK_p,        spawn,           {.v = printfocuscmd} },
   { MODKEY|ShiftMask,  XK_r,        spawn,           {.v = rangercmd} },
   { MODKEY|ShiftMask,  XK_t,        spawn,           {.v = tmuxcmd} },
   { MODKEY|ShiftMask,  XK_y,        spawn,           {.v = todocmd} },
-  { MODKEY|ShiftMask,  XK_e,        spawn,           {.v = espcmd} },
     TAGKEYS(                        XK_1,                      0)
     TAGKEYS(                        XK_2,                      1)
     TAGKEYS(                        XK_3,                      2)
